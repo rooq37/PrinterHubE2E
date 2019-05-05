6f056c4c666d70a6f96ab66ebf0ecd7a7b72f908
Mateusz Rokosa <238501@student.pwr.edu.pl>
2019-05-05 15:17:19 +0200

    URL changed

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
 
-    private static final String URL = "http://printerhub.mateuszs.usermd.net";
+    private static final String URL = "http://printerhub.mtrokosa97.usermd.net";
 
     private static final By selectorMainLogo = By.xpath("//img[@class='main-logo img-fluid']");
     private static final By selectorContent = By.xpath("//div[@class='row']");
