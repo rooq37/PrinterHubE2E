1516eeb6cc1f1bc3f512ff65f30d12a386cf5c06
Mateusz Rokosa <238501@student.pwr.edu.pl>
2019-05-27 00:07:20 +0200

    other users fix

--- src/main/java/pages/OtherUsersPage.java
+++ src/main/java/pages/OtherUsersPage.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class OtherUsersPage extends BasePage {
 
     private static final By selectorSearchButton = By.xpath("//input[@class='btn btn-outline-primary' and @value='Search']");
-    private static final By selectorResultTable = By.xpath("//table[@class='sortable table row-container']//tbody");
+    private static final By selectorResultTable = By.xpath("//table[@class='sortable table']//tbody");
 
     public void enterInTheField(String fieldName, String value){
         String xpath = "//input[@aria-label='" + fieldName + "']";
@@ -26,14 +26,14 @@ public class OtherUsersPage extends BasePage {
     }
 
     public void clickSeeMoreButtonAtRow(String rowContent){
-        By selectorTableRow = By.xpath("//table[@class='sortable table row-container']/tbody/tr");
+        By selectorTableRow = By.xpath("//table[@class='sortable table']/tbody/tr");
         List<WebElement> tableRows = getDriver().findElements(selectorTableRow);
         int index = -1;
         for(int i = 0; i < tableRows.size(); i++)
             if(tableRows.get(i).getText().equals(rowContent)) index = i;
 
         index += 1;
-        By selectorSeeMore = By.xpath("//table[@class='sortable table row-container']/tbody/tr[" + index + "]//a[text()='See more']");
+        By selectorSeeMore = By.xpath("//table[@class='sortable table']/tbody/tr[" + index + "]//a[text()='See more']");
         getDriver().findElement(selectorSeeMore).click();
     }
 
